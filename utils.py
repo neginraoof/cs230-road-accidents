c3475cd0352c3accd3f70bc92a7ec7df6ec230ca
Ubuntu <ubuntu@ip-172-31-58-190.us-west-2.compute.internal>
2020-03-06 05:35:18 +0000

    Added fix for large data stats

--- utils.py
+++ utils.py
@@ -3,10 +3,11 @@ import moviepy.editor
 import numpy as np
 import torch
 
+
 parser = argparse.ArgumentParser(description="Video Classification")
 parser.add_argument('--pretrained', default=False, action="store_true")
 parser.add_argument('--crop_videos', default=False, action="store_true")
-
+parser.add_argument('--get_stats', default=False, action="store_true")
 
 def crop_video(data_dirs):
     outputs = []
@@ -33,18 +34,25 @@ def crop_video(data_dirs):
     return outputs
 
 
-def get_stats(data_loader):
+def get_stats(data_loader, device="cpu"):
     N_count = 0
-    inputs = ()
+    mean_s = ()
+    var_s = ()
     for batch_idx, (X, _, __) in enumerate(data_loader):
         X = X.to(dtype=torch.float32)
-        print("x", X.shape)
         N_count += X.shape[0]
-        inputs += (X,)
-    in_cat = torch.cat(inputs, 0)
-
-    mean = in_cat.mean(dim=[0, 2, 3, 4])
-    std = in_cat.std(dim=[0, 2, 3, 4])
+        mean_s += (X.mean(dim=[0, 2, 3, 4]),)
+        var_s += (X.var(dim=[0, 2, 3, 4]),)
+        print("next")
+ 
+    torch.device(device)
+    mean_cat = torch.stack(mean_s)
+    var_cat = torch.stack(var_s)
+    
+    mean = mean_cat.mean(dim=0)
+    var = var_cat.mean(dim=0)
+    std = torch.sqrt(var)
     print("Mean ", mean)
+    print("var ", var)
     print("Std ", std)
     return mean, std
