ca4487d6728496bd13857a724208a0a956e5085e
neginraoof <neginmr@utexas.edu>
2020-03-12 13:19:12 -0700

    Pretrained- fixed layer size

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ np.save('dummy_test.npy', np.array([2]))
 
 # Dataloader parameters
 batch_size = 20
-image_height, image_width = 250, 350  # resize video 2d frame size
+image_height, image_width = 112, 112  # resize video 2d frame size
 n_frames = 15  #number of frames in a video clip
 fps = 1
 num_classes = 4
